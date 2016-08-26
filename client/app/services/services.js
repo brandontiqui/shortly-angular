8b031930ac5a5fecae5861fa4a5e5496df1c322b
brandontiqui <btiqui@gmail.com>
2016-08-26 15:41:13 -0700

    Implemented getAll and addOne methods for Links Factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,30 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+
+  var getAll = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    }).then(function(response) {
+      return response.data;
+    });
+  };
+
+  var addOne = function(url) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: url
+    }).then(function(response) {
+      return response;
+    });
+  };
+
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
