6cb2bcb690c932e10fc496428e5cc493119181f7
brandontiqui <btiqui@gmail.com>
2016-08-27 12:34:40 -0700

    Add display links whenever they are shortened

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,9 +3,17 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {url: ''};
+  $scope.urls = [];
+
   $scope.addLink = function() {
     console.log(typeof $scope.link.url);
     console.log($scope.link.url);
-    Links.addOne($scope.link.url);
+    Links.addOne($scope.link.url)
+      .then(function(response) {
+        console.log('response from addLink', response);
+        $scope.urls.push(response);
+        console.log($scope.urls);
+        console.log($scope.urls[0].data.title);
+      });
   };
 });
