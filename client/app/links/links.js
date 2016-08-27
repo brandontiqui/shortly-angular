f7ebcd6c7d158efc7942d0ed619577f496183869
brandontiqui <btiqui@gmail.com>
2016-08-27 16:12:21 -0700

    Implement live search for urls

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,8 +3,12 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   $scope.data = {};
+  $scope.data.filter = '';
+  $scope.data.number = 3;
   Links.getAll().then(function(response) {
-    $scope.data.links = response;
+    $scope.data.links = response.sort(function(a, b) {
+      return b.visits - a.visits;
+    });
     console.log('links data links array: ', $scope.data.links);
   });
 
